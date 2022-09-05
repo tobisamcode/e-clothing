bfef1b4619f4aa6b2815bf06f886df4503b6b3a3
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-05 18:52:52 +0100

    building pages and routes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/shop" element={<ShopPage />} />
