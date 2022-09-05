fd37c203ee24c9301545d481c28ba1c2a473a27d
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-05 15:50:46 +0100

    routes

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,11 @@
 import "./menu-item.styles.scss";
+import { useNavigate } from "react-router-dom";
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  const navigate = useNavigate();
+
   return (
-    <div className={`${size} menu-item`}>
+    <div className={`${size} menu-item`} onClick={() => navigate(`${linkUrl}`)}>
       <div
         style={{
           backgroundImage: `url(${imageUrl})`
