2949dc1dc9d36a3a5a4da622cd40956a6dd83c2b
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-05 15:04:09 +0100

    routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const HatsPage = () =>
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>;
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="/hats" element={<HatsPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
