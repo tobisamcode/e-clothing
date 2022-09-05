34b309d52b777acefec92556e778aefc103b6d85
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-05 18:00:27 +0100

    building pages and routes

--- src/components/preview-collection/collection-preview.component.jsx
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -6,9 +6,11 @@ const CollectionPreview = ({ title, items }) => {
       <h1 className="title">{title.toUpperCase()}</h1>
 
       <div className="preview">
-        {items.map((item) => (
-          <div key={item.id}>{item.name}</div>
-        ))}
+        {items
+          .filter((item, idx) => idx < 4)
+          .map((item) => (
+            <div key={item.id}>{item.name}</div>
+          ))}
       </div>
     </div>
   );
