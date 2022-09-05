148a4578315c6f29c585bce0dc28ada98043b207
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-06 00:50:22 +0100

    sign in and sign up components been added

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 
 class SignIn extends React.Component {
   constructor() {
@@ -48,8 +49,7 @@ class SignIn extends React.Component {
             label="password"
             required
           />
-
-          <input type="submit" value="Submit Form" />
+          <CustomButton type="submit"> Sign in </CustomButton>
         </form>
       </div>
     );
