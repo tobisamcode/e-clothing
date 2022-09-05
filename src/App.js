205830003836c63c9160ea03ebdb7b6f3e61d957
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-05 16:42:34 +0100

    building pages and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-const HatsPage = () =>
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>;
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
@@ -13,7 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/hats" element={<HatsPage />} />
+          <Route path="/shop" element={<ShopPage />} />
         </Routes>
       </BrowserRouter>
     </div>
