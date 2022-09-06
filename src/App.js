1ca23432313d615e5984d336684a672ef134acc8
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-06 23:59:36 +0100

    firebase authentication

--- src/App.js
+++ src/App.js
@@ -17,6 +17,19 @@ class App extends React.Component {
     };
   }
 
+  unSubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unSubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    });
+  }
+  componentWillUnmount() {
+    this.unSubscribeFromAuth();
+  }
+
   render() {
     return (
       <div className="App">
