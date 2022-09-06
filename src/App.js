97af387916fa029a89bcc919b3522f672cf9fb69
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-06 23:25:43 +0100

    firebase authentication

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -5,19 +6,31 @@ import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSingUpPage from "./pages/sign-in-&-sign-up/sign-in-&-sign-up.component";
 
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/shop" element={<ShopPage />} />
-          <Route path="/signin" element={<SignInAndSingUpPage />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+import { auth } from "./firebase/firebase.utils";
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      current
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/shop" element={<ShopPage />} />
+            <Route path="/signin" element={<SignInAndSingUpPage />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
