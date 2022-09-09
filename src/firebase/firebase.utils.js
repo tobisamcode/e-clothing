a06beb8e4348732f0ae93f5c6b5b59ec581c7579
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-10 00:16:09 +0100

    on redux

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
+
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 
