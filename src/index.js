b739dabfa55d507826663738407e9850f3c53295
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-10 01:24:33 +0100

    working with the react-redux basics

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 
+import store from "./redux/store";
+
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider>
+  <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
