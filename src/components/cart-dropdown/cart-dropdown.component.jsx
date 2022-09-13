cffea725ea534dfba713d07273e2f17ec514a181
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-13 02:57:14 +0100

    using the selector

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { selectCartItems } from "../../redux/cart/cart.selectors";
 import CartItem from "../cart-item/cart-item";
 import CustomButton from "../custom-button/custom-button.component";
@@ -7,14 +8,23 @@ import CustomButton from "../custom-button/custom-button.component";
 import "./cart-dropdown.styles.scss";
 
 const CartDropdown = ({ cartItems }) => {
+  // const location = useLocation();
+  const navigate = useNavigate();
+
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
-        {cartItems.map((cartItem) => (
-          <CartItem key={cartItem.id} item={cartItem} />
-        ))}
+        {cartItems.length ? (
+          cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} item={cartItem} />
+          ))
+        ) : (
+          <span className="empty-message">Your cart is empty</span>
+        )}
       </div>
-      <CustomButton>GO TO CHECKOUT</CustomButton>
+      <CustomButton onClick={() => navigate("/checkout")}>
+        GO TO CHECKOUT
+      </CustomButton>
     </div>
   );
 };
