2ac2e4632bc3f8ba687ff7cf7b34206828e71073
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-13 15:00:31 +0100

    clearing items from cart

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -16,11 +16,11 @@ const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
       </div>
       <span className="name">{name}</span>
       <span className="quantity">
-        <div className="arrow" onClick={(item) => removeItem(item)}>
+        <div className="arrow" onClick={() => removeItem(cartItem)}>
           &#10094;
         </div>
         <span className="value"> {quantity}</span>
-        <div className="arrow" onClick={(item) => addItem(item)}>
+        <div className="arrow" onClick={() => addItem(cartItem)}>
           &#10095;
         </div>
       </span>
