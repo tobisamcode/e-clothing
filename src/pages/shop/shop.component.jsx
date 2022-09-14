18443f2354413bd09a22bb9050ff9d22d988ac16
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-14 23:39:42 +0100

    using the redux to store the directory data

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 import CollectionPreview from "../../components/preview-collection/collection-preview.component";
+import { selectShopItems } from "../../redux/shop/shop.selectors";
 
-import SHOP_DATA from "./shop.data";
+const ShopPage = ({ collections }) => {
+  return (
+    <div className="shop-page">
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+};
 
-class ShopPage extends React.Component {
-  constructor() {
-    super();
+const mapStatesToProps = createStructuredSelector({
+  collections: selectShopItems
+});
 
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
-
-export default ShopPage;
+export default connect(mapStatesToProps)(ShopPage);
