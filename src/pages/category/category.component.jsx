c497890842312e877f93440945892189ad041339
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-15 00:50:39 +0100

    working with the nested routes in the shop page components

---
+++ src/pages/category/category.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./category.styles.scss";
+
+export const CategoryPage = () => {
+  return (
+    <div className="category">
+      <h2>CATEGORY PAGE</h2>
+    </div>
+  );
+};
