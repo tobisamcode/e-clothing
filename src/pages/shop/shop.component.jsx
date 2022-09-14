4004bae88858bf465dd3c886b2c95434ce7b284c
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-15 00:04:13 +0100

    using the redux to store the shop data

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,21 +1,12 @@
 import React from "react";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
-import CollectionPreview from "../../components/preview-collection/collection-preview.component";
-import { selectCollections } from "../../redux/shop/shop.selectors";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 
 const ShopPage = ({ collections }) => {
   return (
     <div className="shop-page">
-      {collections.map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))}
+      <CollectionsOverview />
     </div>
   );
 };
 
-const mapStatesToProps = createStructuredSelector({
-  collections: selectCollections
-});
-
-export default connect(mapStatesToProps)(ShopPage);
+export default ShopPage;
