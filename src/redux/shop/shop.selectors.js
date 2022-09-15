5911e15b26a1bbc3e8c9130a4fefb70edea41c28
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-15 22:42:30 +0100

    working with the nested routes in the shop page components

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -7,8 +7,7 @@ export const selectCollections = createSelector(
   shop => shop.collections
 );
 
-export const selectCollection = collectionUrlParam =>
-  createSelector(
-    [selectCollections],
-    collections => collections[collectionUrlParam]
-  );
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key])
+);
