15f3ddcdac968b173d6d5801d12a85dadd100cba
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-15 03:13:49 +0100

    working with the nested routes in the shop page components

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { connect, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { selectCollection } from "../../redux/shop/shop.selectors";
 import "./collection.styles.scss";
 
 const CollectionPage = () => {
+  const { collectionId } = useParams();
+
+  const collection = useSelector((state) =>
+    selectCollection(collectionId)(state)
+  );
+
+  console.log(collection);
   return (
     <div className="collection-page">
       <h2>COLLECTION PAGE</h2>
