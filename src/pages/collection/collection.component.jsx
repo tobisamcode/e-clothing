752aed0829871e155ab5ef8105bfbec725125511
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-15 01:11:46 +0100

    working with the nested routes in the shop page components

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./collection.styles.scss";
 
-export const CollectionPage = () => {
+const CollectionPage = () => {
   return (
     <div className="collection-page">
       <h2>COLLECTION PAGE</h2>
     </div>
   );
 };
+
+export default CollectionPage;
