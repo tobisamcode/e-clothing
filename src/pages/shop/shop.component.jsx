8964005aee6ceb22f12e9d287092255bad3fd954
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-15 01:11:50 +0100

    working with the nested routes in the shop page components

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useParams, Route, Routes } from "react-router-dom";
 
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
-import { CollectionPage } from "../collection/collection.component";
+import CollectionPage from "../collection/collection.component";
 
 const ShopPage = () => {
   const params = useParams();
