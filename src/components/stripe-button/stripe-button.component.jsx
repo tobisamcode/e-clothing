f85bc31918311e1b1eed8dc479c6dd4d55415cea
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-17 10:12:55 +0100

    integrating the stripe payment service

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51LiodmIOIGEpFzoVVHAimrH5lvrKEMItN2OCJ1bkKV450XdoM2UzYwLXaC6nxPVwpCMPn98wWpQ2xtmW2yEMDzEs00tN1vEJFe";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="E-clothing"
+      billingAddress
+      shippingAddress
+      image="https://sendeyo.com/up/d/f3eb2117da"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
