1a1ed6398fd324436bd019e55f0aea0a72222ae7
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-17 21:21:14 +0100

    using the styled components in our applications

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,21 +1,7 @@
 import "./custom-button.scss";
 
-const CustomButton = ({
-  children,
-  isGoogleSignIn,
-  inverted,
-  ...otherProps
-}) => {
-  return (
-    <button
-      className={`${inverted ? "inverted" : ""} ${
-        isGoogleSignIn ? "google-sign-in" : ""
-      } custom-button`}
-      {...otherProps}
-    >
-      {children}
-    </button>
-  );
+const CustomButton = ({ children, ...props }) => {
+  return <CustomButton>{children}</CustomButton>;
 };
 
 export default CustomButton;
