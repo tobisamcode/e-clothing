9905a531ae482401f225392f03919cfc104abf86
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-17 17:36:51 +0100

    using the styled components in our applications

---
+++ src/components/header/header.styles.jsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { css } from "styled-components";
+
+export const OptionContainerStyles = css`
+  padding: 10px 15px;
+  cursor: pointer;
+`;
+
+export const HeaderContainer = styled.div`
+  height: 50px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 25px;
+  border-bottom: 1px solid rgb(204, 197, 197);
+`;
+
+export const LogoContainer = styled(Link)`
+  height: 100%;
+  width: 70px;
+  padding: 25px;
+`;
+
+export const OptionContainer = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+export const OptionLink = styled(Link)`
+  ${OptionContainerStyles}
+`;
+export const OptionDiv = styled.div`
+  ${OptionContainerStyles}
+`;
