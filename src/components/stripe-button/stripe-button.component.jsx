660ef0d501dc4f73606dc1255c816a38e96467fe
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-17 10:16:02 +0100

    integrating the stripe payment service

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -17,7 +17,7 @@ const StripeCheckoutButton = ({ price }) => {
       name="E-clothing"
       billingAddress
       shippingAddress
-      image="https://sendeyo.com/up/d/f3eb2117da"
+      image="https://svgshare.com/i/CUz.svg"
       description={`Your total is $${price}`}
       amount={priceForStripe}
       panelLabel="Pay Now"
