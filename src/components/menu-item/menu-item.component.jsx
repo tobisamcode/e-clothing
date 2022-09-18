257df503ce3ff1e9fe51cd6f7c9a2ac2b5e12356
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-18 23:26:50 +0100

    using the styled components in our applications

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,22 +1,28 @@
-import "./menu-item.styles.scss";
 import { useNavigate } from "react-router-dom";
+import {
+  ContentContainer,
+  ContentSubtitle,
+  ContentTitle,
+  ImageBackgroundContainer,
+  MenuItemContainer
+} from "./menu-item.styles";
 
 const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
   const navigate = useNavigate();
 
   return (
-    <div className={`${size} menu-item`} onClick={() => navigate(`${linkUrl}`)}>
-      <div
+    <MenuItemContainer onClick={() => navigate(`${linkUrl}`)}>
+      <ImageBackgroundContainer
         style={{
           backgroundImage: `url(${imageUrl})`
         }}
         className="background-image"
-      ></div>
-      <div className="content">
-        <h1 className="title">{title.toUpperCase()}</h1>
-        <span className="subtitle">SHOP NOW</span>
-      </div>
-    </div>
+      ></ImageBackgroundContainer>
+      <ContentContainer>
+        <ContentTitle>{title.toUpperCase()}</ContentTitle>
+        <ContentSubtitle>SHOP NOW</ContentSubtitle>
+      </ContentContainer>
+    </MenuItemContainer>
   );
 };
 
