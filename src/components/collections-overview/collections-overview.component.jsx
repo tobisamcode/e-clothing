9e514eb6dae6d94b2856bc72c247bd349915b20c
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-18 22:57:28 +0100

    using the styled components in our applications

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { connect } from "react-redux";
-import "./collections-overview.styles.scss";
 import CollectionPreview from "../preview-collection/collection-preview.component";
 import { createStructuredSelector } from "reselect";
 import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
+import { CollectionsOverviewContainer } from "./collections-overview.styles";
 
 const CollectionsOverview = ({ collections }) => {
   return (
-    <div>
+    <CollectionsOverviewContainer>
       {collections.map(({ id, ...otherCollectionProps }) => (
         <CollectionPreview key={id} {...otherCollectionProps} />
       ))}
-    </div>
+    </CollectionsOverviewContainer>
   );
 };
 
