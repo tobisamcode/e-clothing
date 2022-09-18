8baf1d823cac63bffe23e54bf45dc7fe9711aba4
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-18 22:25:50 +0100

    using the styled components in our applications

--- src/components/cart-item/cart-item.jsx
+++ src/components/cart-item/cart-item.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-
-import "./cart-item.styles.scss";
+import { CartItemContainer, ItemDetails } from "./cart-item.styles";
 
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
   return (
-    <div className="cart-item">
+    <CartItemContainer>
       <img src={imageUrl} alt="item" />
-      <div className="item-details">
+      <ItemDetails>
         <span className="name">{name}</span>
         <span className="price">
           {quantity} x ${price}
         </span>
-      </div>
-    </div>
+      </ItemDetails>
+    </CartItemContainer>
   );
 };
 
