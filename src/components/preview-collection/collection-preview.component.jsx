6b96b06b2854db7fd0b793d881d955f5794ab5b7
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-18 23:45:06 +0100

    using the styled components in our applications

--- src/components/preview-collection/collection-preview.component.jsx
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -1,19 +1,23 @@
 import CollectionItem from "../collection-item/collection-item.component";
-import "./collection-preview.styles.scss";
+import {
+  CollectionPreviewContainer,
+  PreviewContainer,
+  TitleContainer
+} from "./collection-preview.styles";
 
 const CollectionPreview = ({ title, items }) => {
   return (
-    <div className="collection-preview">
-      <h1 className="title">{title.toUpperCase()}</h1>
+    <CollectionPreviewContainer>
+      <TitleContainer>{title.toUpperCase()}</TitleContainer>
 
-      <div className="preview">
+      <PreviewContainer>
         {items
           .filter((item, idx) => idx < 4)
           .map((item) => (
             <CollectionItem key={item.id} item={item} />
           ))}
-      </div>
-    </div>
+      </PreviewContainer>
+    </CollectionPreviewContainer>
   );
 };
 
