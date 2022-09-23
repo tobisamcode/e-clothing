494cae90fe1807a3044ee576aa241a7204076909
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-23 23:31:32 +0100

    storing our app database in the firestore

--- src/App.js
+++ src/App.js
@@ -40,7 +40,6 @@ class App extends React.Component {
 
       setCurrentUser(userAuth);
       addCollectionAndDocuments("collections", collections);
-      console.log(collections);
     });
   }
   componentWillUnmount() {
