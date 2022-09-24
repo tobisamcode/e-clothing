b398c73330698a4886643b47667b35dbcb1a29ce
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-24 01:12:36 +0100

    storing our app database in the firestore

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,15 +4,35 @@ import { Route, Routes } from "react-router-dom";
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../collection/collection.component";
 
-const ShopPage = () => {
-  return (
-    <div className="shop-page">
-      <Routes>
-        <Route path="/" element={<CollectionsOverview />} />
-        <Route path="/:collectionId" element={<CollectionPage />} />
-      </Routes>
-    </div>
-  );
-};
+import {
+  firestore,
+  convertCollectionsSnapshotToMap
+} from "../../firebase/firebase.utils";
+
+class ShopPage extends React.Component {
+  unsubscribeFromSnapshot = null;
+
+  componentDidMount() {
+    const collectionRef = firestore.collection("collections");
+
+    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
+      async (snapshot) => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        console.log(collectionsMap);
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div className="shop-page">
+        <Routes>
+          <Route path="/" element={<CollectionsOverview />} />
+          <Route path="/:collectionId" element={<CollectionPage />} />
+        </Routes>
+      </div>
+    );
+  }
+}
 
 export default ShopPage;
