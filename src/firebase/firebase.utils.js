21e738a567b29585708d4d53a0e74ef94ede3f25
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-24 01:12:29 +0100

    storing our app database in the firestore

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -57,6 +57,24 @@ export const addCollectionAndDocuments = async (
   return await batch.commit();
 };
 
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
+
 const firebaseApp = initializeApp(config);
 
 firebase.initializeApp(config);
