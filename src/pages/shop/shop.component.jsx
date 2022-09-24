1926ea231432691afc50539da55f9b11d82baf97
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-24 01:32:37 +0100

    storing our app database in the firestore

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -8,17 +8,20 @@ import {
   firestore,
   convertCollectionsSnapshotToMap
 } from "../../firebase/firebase.utils";
+import { connect } from "react-redux";
+import { updateCollections } from "../../redux/shop/shops.actions";
 
 class ShopPage extends React.Component {
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
+    const { updateCollections } = this.props;
     const collectionRef = firestore.collection("collections");
 
     this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
       async (snapshot) => {
         const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-        console.log(collectionsMap);
+        updateCollections(collectionsMap);
       }
     );
   }
@@ -35,4 +38,9 @@ class ShopPage extends React.Component {
   }
 }
 
-export default ShopPage;
+const mapDispatchToProps = (dispatch) => ({
+  updateCollections: (collectionsMap) =>
+    dispatch(updateCollections(collectionsMap))
+});
+
+export default connect(mapDispatchToProps)(ShopPage);
