1116a49dff5be275d2cbb41bde1e492002583f8a
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-09-29 00:58:34 +0100

    storing our app database in the firestore

---
+++ src/components/spinner/spinner.components.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { SpinnerContainer, SpinnerOverlay } from "./spinner.styles";
+
+const Spinner =
+  (WrappedComponent) =>
+  ({ isLoading, ...otherProps }) => {
+    return isLoading ? (
+      <SpinnerOverlay>
+        <SpinnerContainer />
+      </SpinnerOverlay>
+    ) : (
+      <WrappedComponent {...otherProps} />
+    );
+  };
+
+export default Spinner;
